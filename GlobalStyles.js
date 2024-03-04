14b8f5e2824ef4bef79ebf8220ccf2c1054c250c
ley_ <lltenebroso@up.edu.ph>
2024-03-04 19:54:40 +0800

    static provider profile screen

---
+++ GlobalStyles.js
@@ -0,0 +1,27 @@
+/* fonts */
+export const FontFamily = {
+    quicksandRegular: "Quicksand-Regular",
+    quicksandSemiBold: "Quicksand-SemiBold",
+    quicksandBold: "Quicksand-Bold",
+  };
+  /* font sizes */
+  export const FontSize = {
+    size_sm: 14,
+    size_6xl: 25,
+    size_11xl: 30,
+    size_21xl: 40,
+  };
+  /* Colors */
+  export const Color = {
+    colorWhite: "white",
+    colorDarkgray: "#9a9797",
+    colorBlack: "#000",
+    colorSilver: "#c4c4c4",
+    colorDarkslategray_100: "#05364c",
+    
+  };
+  /* border radiuses */
+  export const Border = {
+    br_xl: 20,
+  };
+  
