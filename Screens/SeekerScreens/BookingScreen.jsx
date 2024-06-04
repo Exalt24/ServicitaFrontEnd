588bdf5bb19bd48d43f76bb90e418426d298e841
ley_ <lltenebroso@up.edu.ph>
2024-06-05 03:10:31 +0800

    Update BookingScreen.jsx

--- Screens/SeekerScreens/BookingScreen.jsx
+++ Screens/SeekerScreens/BookingScreen.jsx
@@ -1,3 +1,4 @@
+//BookingScreen
 import React, { useState, useEffect } from 'react';
 import { useFocusEffect } from '@react-navigation/native'
 import { StyleSheet, View, Text, Dimensions, TextInput, TouchableOpacity, Image } from "react-native";
